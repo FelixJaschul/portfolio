2fdc07a156050ca0faa6294deecccc121bcfa9b5
Felistig <129108886+FelixJaschul@users.noreply.github.com>
2025-12-25 09:57:10 +0100

    Update scripts.js with improved createImGuiWindow function

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const createImGuiWindow = () => {
+    // Import ImGui if not yet present, load library (pseudo-runtime/fileRef convenience)
+    // Debug Build/deployed Modern-Hooks w/plugins Core
+
+};
